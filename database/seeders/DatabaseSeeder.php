076c398fe720bdeb605f59f69b242743c1e120ef
Samuel Khalaf <eng.samuel.khalaf@gmail.com>
2025-09-17 22:56:28 +0300

    fix migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(DocumentTypeSeeder::class);
+        // Check if running on Railway (production environment)
+        // if (app()->environment('production') || env('RAILWAY_ENVIRONMENT')) {
+        //     $this->call(RailwaySeeder::class);
+        // } else {
+        //     // Local development - run all seeders
+        //     $this->call(RoleSeeder::class);
+        //     $this->call(UserSeeder::class);
+        //     $this->call(DocumentTypeSeeder::class);
+            $this->call(BasicDataSeeder::class);
+        // }
     }
 }
